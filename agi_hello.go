020f1b8bcd5a206b6b8b873bbbb46f6760a4908f
Lefteris Zafiris <zaf.000@gmail.com>
2013-07-29 14:32:13 +0100

    agi_hello fixes

--- agi_hello.go
+++ agi_hello.go
@@ -1,5 +1,8 @@
 // A simple AGI example in go
+// We read and store AGI vars, run some simple AGI commands and parse the output
+//
 // Copyright (C) 2013, Lefteris Zafiris <zaf.000@gmail.com>
+//
 
 package main
 
@@ -9,20 +12,19 @@ import "bufio"
 import "strings"
 
 var debug = true
-var agi_data map[string]string
 var agi_reader = bufio.NewReaderSize(os.Stdin, 0)
 
 func main() {
-  var res []string
-	agi_data = make(map[string]string)
-	
+	var res []string
+	agi_data := make(map[string]string)
+
 	agi_init(agi_data)
 	if agi_data["agi_arg_1"] == "" {
 		fmt.Fprintln(os.Stderr,"No arguments passed, exiting")
 		os.Exit(1)
 	}
 	my_file := agi_data["agi_arg_1"]
-	
+
 	//Check channel status
 	fmt.Fprintln(os.Stdout, "CHANNEL STATUS")
 	res = agi_response()
@@ -32,7 +34,7 @@ func main() {
 		res = agi_response()
 		if res[1] == "-1" {
 			fmt.Fprintln(os.Stderr,"Failed to answer channel")
-			os.Exit(1)	
+			os.Exit(1)
 		}
 	}
 	//Display on the console the file we are about to playback
@@ -52,7 +54,7 @@ func main() {
 	os.Exit(0)
 }
 
-func agi_init(map[string]string) {
+func agi_init(agi_in map[string]string) {
 	//Read and store AGI input
 	for {
         line, err := agi_reader.ReadString('\n')
@@ -61,24 +63,24 @@ func agi_init(map[string]string) {
         }
 		input_str := strings.SplitN(line, ": ", 2)
 		if len(input_str) == 2 {
-			agi_data[input_str[0]] = strings.Replace(input_str[1], "\n", "", -1)
+			agi_in[input_str[0]] = strings.Replace(input_str[1], "\n", "", -1)
 		}
     }
 
 	if debug {
 		fmt.Fprintln(os.Stderr, "Finished reading AGI vars:")
-		for key, value := range agi_data {
+		for key, value := range agi_in {
 			fmt.Fprintln(os.Stderr, "Key:", key, "Value:", value)
 		}
 	}
-}	
+}
 
 func agi_response() ([]string) {
 	// Read back AGI repsonse
 	line, _ := agi_reader.ReadString('\n')
 	res := strings.Replace(line, "\n", "", -1)
 	reply := strings.Fields(res)
-	
+
 	if len(reply) < 2 {
 		fmt.Fprintln(os.Stderr,"AGI unexpected error!")
 		return []string{"-1", "-1", "-1"}
